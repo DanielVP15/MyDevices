a53321cf3a7fbcf28ca1c0bf055aab7665c4faeb
dpires <dvpires@stefanini.com>
2018-01-25 08:12:37 -0300

    Objeto User adicionado

---
+++ app/src/main/java/com/rfp/dvp/mydevices/devices/User.java
@@ -0,0 +1,54 @@
+package com.rfp.dvp.mydevices.devices;
+
+/**
+ * Created by dvpires on 24/01/2018.
+ */
+
+public class User {
+
+    private String id;
+    private String email;
+    private String name;
+    private String device;
+
+    public User() {
+    }
+
+    public User(String email,String name) {
+        setEmail(email);
+        setName(name);
+        setDevice(null);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+}
